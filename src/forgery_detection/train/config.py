c7208f3441f4f804adac44602d9ff867ffa3300f
Sebastian Fellner <fellnerseb@gmail.com>
2019-10-15 17:51:09 +0200

    sampling works

---
+++ src/forgery_detection/train/config.py
@@ -0,0 +1,30 @@
+import random
+
+from torch.optim import Adam
+
+from forgery_detection.models.simple_vgg import VGG11Binary
+
+simple_vgg = {
+    "config": {
+        "settings": {
+            "use_gpu": False,
+            "epoch_size": 2,
+            "test_size": 1,
+            "batch_size": 8,
+        },
+        "model": VGG11Binary,
+        "optimizer": Adam,
+        "hyper_parameter": {  # todo put this in config as well
+            "optimizer": {
+                "lr": lambda _: random.uniform(
+                    0.001, 0.1
+                ),  # this is just for initializing the trials
+                "weight_decay": lambda _: random.uniform(0.1, 0.9),
+            }
+        },
+    },
+    "stop": {"mean_accuracy": 1.1, "training_iteration": 40},
+    "resources_per_trial": {"cpu": 2, "gpu": 0},
+    "num_samples": 4,
+    "checkpoint_freq": 5,
+}
--- src/forgery_detection/train/main.py
+++ src/forgery_detection/train/main.py
@@ -1,32 +1,33 @@
-import argparse
-import random
 from datetime import datetime
+from types import LambdaType
 
 import numpy as np
 import ray
 from ray import tune
+from ray.tune import sample_from
 from ray.tune.logger import DEFAULT_LOGGERS
 from ray.tune.schedulers import PopulationBasedTraining
-from torch.optim import Adam
 
 from forgery_detection.data.face_forensics.utils import get_data_loaders
-from forgery_detection.models.simple_vgg import VGG11Binary
 from forgery_detection.models.simple_vgg import VGgTrainable
+from forgery_detection.train.config import simple_vgg
 
-# Training settings
-parser = argparse.ArgumentParser(description="Small vgg on faceforensic dataset")
-parser.add_argument(
-    "--use-gpu", action="store_true", default=False, help="enables CUDA training"
-)
-parser.add_argument(
-    "--smoke-test", action="store_true", help="Finish quickly for testing"
-)
+
+def sample(hyper_parameter: dict) -> dict:
+    sampled_dict = {}
+    for key, value in hyper_parameter.items():
+        if isinstance(value, dict):
+            sampled_dict[key] = sample(value)
+        elif isinstance(value, LambdaType):
+            sampled_dict[key] = sample_from(value)
+        else:
+            sampled_dict[key] = value
+    return sampled_dict
 
 
 def main():
     np.random.seed(42)
 
-    args = parser.parse_args()
     ray.init()
     batch_size = 8  # todo take this from the config
     train_loader, test_loader = get_data_loaders(
@@ -34,44 +35,19 @@ def main():
     )  # todo thin about what acutally needs ot be pinned
     # X_id = pin_in_object_store(np.random.random(size=100000000))
 
-    train_spec = {
-        "config": {
-            "settings": {
-                "args": args,
-                "epoch_size": 2,
-                "test_size": 1,
-                "batch_size": batch_size,
-                "train_loader_id": ray.put(train_loader),
-                "test_loader_id": ray.put(test_loader),
-            },
-            "model": VGG11Binary,
-            "optimizer": Adam,
-            "hyper_parameter": {
-                "optimizer": {
-                    "lr": tune.uniform(
-                        0.001, 0.1
-                    ),  # this is just for initializing the trials
-                    "weight_decay": tune.uniform(0.1, 0.9),
-                }
-            },
-        }
-    }
+    train_spec = simple_vgg.copy()
+    train_spec["config"]["settings"]["train_loader_id"] = ray.put(train_loader)
+    train_spec["config"]["settings"]["test_loader_id"] = ray.put(test_loader)
+
+    hyper_parameter = train_spec["config"]["hyper_parameter"].copy()
+    train_spec["config"]["hyper_parameter"] = sample(hyper_parameter)
 
     pbt = PopulationBasedTraining(
         time_attr="training_iteration",
         metric="mean_accuracy",
         mode="max",
         perturbation_interval=2,
-        hyperparam_mutations={
-            "hyper_parameter": {  # todo put this in config as well
-                "optimizer": {
-                    "lr": lambda: random.uniform(
-                        0.001, 0.1
-                    ),  # this is just for initializing the trials
-                    "weight_decay": lambda: random.uniform(0.1, 0.9),
-                }
-            }
-        },
+        hyperparam_mutations={"hyper_parameter": hyper_parameter},
     )
     experiment_name = f"vgg_experiments/{datetime.now()}"
     analysis = tune.run(  # todo put most of the stuff in config
@@ -79,17 +55,9 @@ def main():
         scheduler=pbt,
         reuse_actors=True,
         verbose=True,
-        stop={
-            "mean_accuracy": 1.1,  # 0.95,
-            "training_iteration": 40 if args.smoke_test else 20,  # put this in config
-        },
-        resources_per_trial={"cpu": 2, "gpu": int(args.use_gpu)},  # put this in config
-        num_samples=4 if args.smoke_test else 20,  # put this in config
-        checkpoint_at_end=True,
-        checkpoint_freq=5,  #
         loggers=DEFAULT_LOGGERS,
         name=experiment_name,
-        local_dir="./log",
+        local_dir="../log",
         **train_spec,
     )
     print(analysis.trials)
