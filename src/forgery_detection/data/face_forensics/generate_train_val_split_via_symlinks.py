1011efa77a115d9c9bf972982dab420c7db24687
Sebastian Fellner <fellnerseb@gmail.com>
2019-10-27 13:47:44 +0100

    implement train val test split from faceforensics++

--- src/forgery_detection/data/face_forensics/generate_train_val_split_via_symlinks.py
+++ src/forgery_detection/data/face_forensics/generate_train_val_split_via_symlinks.py
@@ -1,129 +1,52 @@
-import shutil
 from pathlib import Path
 
 import click
 
-
-def _symlink_folders_and_do_val_split(
-    source_dir: Path,
-    target_dir: Path,
-    target_val_dir: Path,
-    compression="raw",
-    val_percentage=0.2,
-):
-    target_dir.mkdir(parents=True, exist_ok=True)
-    target_val_dir.mkdir(parents=True, exist_ok=True)
-
-    for manipulation_method in source_dir.iterdir():
-        if manipulation_method.is_dir():
-            images_folder = manipulation_method / compression / "images"
-
-            if images_folder.exists():
-                print("processing", manipulation_method)
-                (target_dir / manipulation_method.name).mkdir(parents=True)
-                for video_folder in images_folder.iterdir():
-                    if video_folder.is_dir():
-                        symlink_dir = (
-                            target_dir / manipulation_method.name / video_folder.name
-                        )
-                        symlink_dir.symlink_to(video_folder, target_is_directory=True)
-                _move_symlinks(
-                    target_dir / manipulation_method.name,
-                    target_val_dir,
-                    val_percentage,
-                )
-            else:
-                print("skipping", manipulation_method)
+from forgery_detection.data.face_forensics.splits import TEST
+from forgery_detection.data.face_forensics.splits import TEST_NAME
+from forgery_detection.data.face_forensics.splits import TRAIN
+from forgery_detection.data.face_forensics.splits import TRAIN_NAME
+from forgery_detection.data.face_forensics.splits import VAL
+from forgery_detection.data.face_forensics.splits import VAL_NAME
 
 
-def _move_symlinks(source: Path, target: Path, percentage=0.2):
-    """Move some percentage of videos from one folder to another folder.
-
-    Internally the alphabetically-sorted-last {percentage} videos are moved over.
-
-    Args:
-        source: source folder
-        target: target folder
-        percentage: len(items in source folder)*{precentage} are moved.
-
-    """
-
-    folders = list(source.glob("*"))
-    folders.sort()
-    num_val_videos = int(len(folders) * percentage + 0.5)
-    (target / source.name).mkdir()
-    try:
-        for i in range(num_val_videos):
-            shutil.move(
-                str(folders[-1 - i]), str(target / source.name / folders[-1 - i].name)
-            )
-    except OSError as e:
-        print("probably the permissions of the source folder are read only: ", e)
+def _symlink_split(source_dir_method, target_dir, split):
+    for video in source_dir_method.iterdir():
+        if video.is_dir():
+            target_dir.mkdir(parents=True, exist_ok=True)
+            if video.name.split("_")[0] in split:
+                (target_dir / video.name).symlink_to(video, target_is_directory=True)
 
 
 @click.command()
-@click.option("--source_dir", required=True, type=click.Path(exists=True))
-@click.option("--target_dir", required=True)
+@click.option("--source_dir_root", required=True, type=click.Path(exists=True))
+@click.option("--target_dir_root", required=True)
 @click.option("--compression", default="c40")
-@click.option("--val_percentage", default=0.2)
-def symlink_all_folders_and_do_val_split(
-    source_dir, target_dir, compression, val_percentage
-):
-    """Symlinks folders from source dir to target dir.
+def symlink_train_val_test_split(source_dir_root, target_dir_root, compression):
 
-    source dir has to follow faceforensics folder structure:
+    source_dir_root = Path(source_dir_root)
+    if not source_dir_root.exists():
+        raise FileNotFoundError(f"{source_dir_root} does not exist")
 
-    data:
-        - manipulated_sequences:
-            - a:
-                - {compression}:
-                    - images
-            - b:
-                - {compression}:
-                    - images
-            ...
-        - original_sequences:
-            - a:
-                - {compression}:
-                    - images
-            - b:
-                - {compression}:
-                    - images
-            ...
+    target_dir_root = Path(target_dir_root)
 
-    Will do a train val split, by moving the last video of each method to a val folder.
-
-    resulting structure:
-
-
-    """
-    source_dir = Path(source_dir)
-    target_dir_val = Path(target_dir) / "val"
-    target_dir_train = Path(target_dir) / "train"
-
-    if not source_dir.exists():
-        raise FileNotFoundError(f"{source_dir} does not exist")
-
-    manipulated_sequences_source = source_dir / "manipulated_sequences"
-    manipulated_sequences_target = target_dir_train / "manipulated_sequences"
-    _symlink_folders_and_do_val_split(
-        manipulated_sequences_source,
-        manipulated_sequences_target,
-        target_dir_val / "manipulated_sequences",
-        compression=compression,
-        val_percentage=val_percentage,
-    )
-
-    original_sequences_source = source_dir / "original_sequences"
-    original_sequences_target = target_dir_train / "original_sequences"
-    _symlink_folders_and_do_val_split(
-        original_sequences_source,
-        original_sequences_target,
-        target_dir_val / "original_sequences",
-        compression=compression,
-        val_percentage=val_percentage,
-    )
+    sub_dirs = ["original_sequences/youtube"] + [
+        "manipulated_sequences/" + manipulated_sequence
+        for manipulated_sequence in [
+            "Deepfakes",
+            "Face2Face",
+            "FaceSwap",
+            "NeuralTextures",
+        ]
+    ]
+    for split, split_name in [(TRAIN, TRAIN_NAME), (VAL, VAL_NAME), (TEST, TEST_NAME)]:
+        for sub_dir in sub_dirs:
+            _symlink_split(
+                source_dir_root / sub_dir / compression / "images",
+                target_dir_root / split_name / sub_dir / compression / "images",
+                split,
+            )
 
 
 if __name__ == "__main__":
-    symlink_all_folders_and_do_val_split()
+    symlink_train_val_test_split()
