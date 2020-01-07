c13b5d8031fbcca3db81ad45dd5383004f380447
Sebastian Fellner <fellnerseb@gmail.com>
2020-01-07 17:17:22 +0100

    new hprarams diagram script

--- src/forgery_detection/data/tb_hparams_copy_to_excel.py
+++ src/forgery_detection/data/tb_hparams_copy_to_excel.py
@@ -23,9 +23,15 @@ experiments = []
 experiments_str = []
 
 
-with open("tb_hparams_augmentation.txt") as f:
-    # with open("tb_hparams_lr_weight_decay.txt") as f:
-    text = f.readlines()
+# with open("tb_hparams_augmentation.txt") as f:
+with open("tb_hparams_lr_weight_decay.txt") as f:
+    # remove headers
+    text = "".join(f.readlines()[10:])
+    text = text.replace("\n", "").replace(" ", "")
+
+    # filter out empty lines
+    text = text.split(",")
+    text = list(filter(lambda x: len(x) > 0, text))
 
 for i in range(len(text) // len(header)):
     start_idx = i * len(header)
