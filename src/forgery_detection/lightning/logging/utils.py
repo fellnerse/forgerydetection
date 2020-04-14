d6f528760591d015ccdd863cf8b83bd215ad6c49
Sebastian Fellner <fellnerseb@gmail.com>
2020-04-14 20:11:18 +0200

    update pyptorch lightning remove unused dependencies

--- src/forgery_detection/lightning/logging/utils.py
+++ src/forgery_detection/lightning/logging/utils.py
@@ -72,7 +72,7 @@ def get_logger_and_checkpoint_callback(
 
     checkpoint_callback = ModelCheckpoint(
         filepath=logger_dir / CHECKPOINTS,
-        save_best_only=False,
+        save_top_k=-1,
         monitor=VAL_ACC,
         mode="max",
         prefix="",
